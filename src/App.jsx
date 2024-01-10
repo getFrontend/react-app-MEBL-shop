f244606242eee8e57d6266f07c1b1fd0ec60a0a6
Sergey UP <sergey10goals@gmail.com>
2024-01-10 11:27:14 +0200

    import structure: Header, Main, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Footer } from "./views/Footer/Footer";
+import { Header } from "./views/Header/Header";
+import { Main } from "./views/Main/Main";
 
 function App() {
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
