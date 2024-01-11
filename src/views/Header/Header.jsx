69d01ca8d193d556543081753ffda4d61a60609a
Sergey UP <sergey10goals@gmail.com>
2024-01-11 23:54:27 +0200

    updated Header component

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -1,9 +1,15 @@
+import Logo from "../../components/Logo/Logo";
+import Navigation from "../../components/Navigation/Navigation";
+import SearchForm from "../../components/SearchForm/SearchForm";
 import { Container } from "../Container/Container";
+import "./Header.scss";
 
 export const Header = () => (
-  <header>
-    <Container>
-      <h1>MEBL</h1>
+  <header className="header">
+    <Container className="header__container">
+      <Logo className="header__logo" />
+      <SearchForm className="header__search"/>
+      <Navigation className="header__navigation"/>
     </Container>
   </header>
 );
