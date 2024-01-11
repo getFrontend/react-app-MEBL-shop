ddb58a6fe37a76ee5eed09cf840f96bca0d79432
Sergey UP <sergey10goals@gmail.com>
2024-01-11 23:55:32 +0200

    updated footer component & add styles & add html render

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -1,5 +1,22 @@
+import Contacts from "../../components/Contacts/Contacts";
+import Logo from "../../components/Logo/Logo";
+import { Container } from "../Container/Container";
+import "./Footer.scss";
+
 export const Footer = () => (
-  <footer>
-    <div>Footer section</div>
+  <footer className="footer">
+    <Container className="footer__container">
+      <Logo className="footer__logo" />
+      <Contacts className="footer__contacts" />
+
+      <ul className="footer__developer-list">
+        <li className="footer__developer-item">
+          Developer:
+          <a href="https://t.me/Vietn_am" className="footer__developer-link" target="_blank" rel="noreferrer"> Sergey Up</a>
+        </li>
+      </ul>
+
+      <p className="footer__copyright">© MEBL, 2024</p>
+    </Container>
   </footer>
 );
