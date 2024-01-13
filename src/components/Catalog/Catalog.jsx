1a09d7aa1102e4146a725c20aa3ab344e94a8b27
Sergey UP <sergey10goals@gmail.com>
2024-01-13 17:58:58 +0200

    Changed map to forEachfor a better key formation

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,23 +1,26 @@
 import { Container } from "../../views/Container/Container";
 import styles from "./Catalog.module.scss";
 
-
 function Catalog(props) {
-  let i = 0;
-
   const data = [
     "Тумбы", "Стулья", "Столы", "Пуфы и банкетки", "Кровати", "Диваны", "Полки", "Стеллажи"
   ];
+
+  const dataList = [];
+
+  data.forEach((item, id) => {
+    dataList.push(
+      <li className={styles["catalog__item"]} key={id}>
+        <a className={styles["catalog__link"]} href="#!">{item}</a>
+      </li>
+    );
+  });
 
   return (
     <nav className={styles["catalog"]}>
       <Container>
         <ul className={styles["catalog__list"]}>
-          {data.map(el => (
-            <li className={styles["catalog__item"]} key={i++}>
-              <a className={styles["catalog__link"]} href="#!">{el}</a>
-            </li>
-          ))}
+          {dataList}
         </ul>
       </Container>
     </nav>
