ed0333e1c38e81b240d614ad5bced0469a76455a
Sergey UP <sergey10goals@gmail.com>
2024-01-14 11:54:16 +0200

    Added useDispatch, useSelector, useEffect, data generation for the catalog

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
 import Catalog from "../../components/Catalog/Catalog";
 import Products from "../../components/Products/Products";
 import "./Main.scss";
+import { useEffect } from "react";
+import { fetchCategories } from "../../store/categories/categories.slice";
 
-export const Main = () => (
-  <main>
-    <Catalog />
-    <Products />
-  </main>
-);
+export const Main = () => {
+  const dispatch = useDispatch();
+  const {
+    data: dataCategories,
+    loading: loadingCategories,
+    error: errorCategories
+  } = useSelector(state => state.categories);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  if (loadingCategories) return <div className="loading">Загрузка...</div>;
+  if (errorCategories) return <div>Ошибка: {errorCategories}</div>;
+
+  return (
+    <main>
+      <Catalog data={dataCategories} />
+      <Products />
+    </main>
+  );
+};
+
