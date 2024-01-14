5fbb7f3b875b8a4bae016238299124fdf3aea286
Sergey UP <sergey10goals@gmail.com>
2024-01-14 11:11:35 +0200

    added authCategories

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth.slice";
+import authCategories from "./categories/categories.slice";
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    categories: authCategories
   }
 });
