18030791d6bc257552f402be87a2fb23f5913a4f
Sergey UP <sergey10goals@gmail.com>
2024-01-14 11:51:28 +0200

    updated the component, added data received from the server instead of the default data

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,29 +1,23 @@
 import { Container } from "../../views/Container/Container";
 import styles from "./Catalog.module.scss";
 
-function Catalog(props) {
-  const data = [
-    "Тумбы", "Стулья", "Столы", "Пуфы и банкетки", "Кровати", "Диваны", "Полки", "Стеллажи"
-  ];
-
-  const dataList = [];
-
-  data.forEach((item, id) => {
-    dataList.push(
-      <li className={styles["catalog__item"]} key={id}>
-        <a className={styles["catalog__link"]} href="#!">{item}</a>
-      </li>
-    );
-  });
+function Catalog({ data }, ...props) {
+  // const defaultData = [
+  //   "Тумбы", "Стулья", "Столы", "Пуфы и банкетки", "Кровати", "Диваны", "Полки", "Стеллажи"
+  // ];
 
   return (
     <nav className={styles["catalog"]}>
-      <Container>
+      <Container className={styles["catalog__container"]}>
         <ul className={styles["catalog__list"]}>
-          {dataList}
+          {data.map((item, index) => (
+            <li className={styles["catalog__item"]} key={index}>
+              <a className={styles["catalog__link"]} href={`/category?slug=${item}`}>{item}</a>
+            </li>
+          ))}
         </ul>
       </Container>
-    </nav>
+    </nav >
   );
 }
 
