905fba752563ee1c943a1a6e4250097f651fec09
Sergey UP <sergey10goals@gmail.com>
2024-01-15 00:01:58 +0200

    Added getting products from API

---
+++ src/store/products/products.slice.js
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const API_URL_PRODUCTS = "https://koff-api.vercel.app/api/products";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const token = state.auth.accessToken;
+
+    const response = await fetch(API_URL_PRODUCTS, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error("Ошибка получения данных! Не удалось получить список товаров!");
+    }
+
+    return response.json();
+  }
+);
+
+const initialState = {
+  data: [],
+  loading: false,
+  error: null
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  }
+});
+
+export default productsSlice.reducer;
