a40b0bd011998bbd03b14028980bbf176e5579b5
Sergey UP <sergey10goals@gmail.com>
2024-01-15 00:03:06 +0200

    added authProducts

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth.slice";
 import authCategories from "./categories/categories.slice";
+import authProducts from "./products/products.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    categories: authCategories
+    categories: authCategories,
+    products: authProducts
   }
 });
