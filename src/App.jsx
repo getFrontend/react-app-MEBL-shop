0ef88b586355b7aa3f190190f685063fa69f287b
Sergey UP <sergey10goals@gmail.com>
2024-01-14 10:44:38 +0200

    added useDispatch, useSelector, useEffect and loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 import { Footer } from "./views/Footer/Footer";
 import { Header } from "./views/Header/Header";
 import { Main } from "./views/Main/Main";
+import { useEffect } from "react";
+import { fetchAccessToken } from "./store/auth/auth.slice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { accessToken, loading } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (!accessToken) {
+      dispatch(fetchAccessToken());
+    }
+  }, [dispatch, accessToken]);
+
   return (
     <>
       <Header />
-      <Main />
+      {!loading && accessToken ? <Main /> : <div className="loading">Загрузка...</div>}
       <Footer />
     </>
   );
