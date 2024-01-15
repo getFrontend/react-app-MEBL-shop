de1e6675c1b8eebce1f3170dccba9e809378235c
Sergey UP <sergey10goals@gmail.com>
2024-01-15 09:27:26 +0200

    Changed the order of the imports, putting style reset via normalize.css first.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "normalize.css";
 import "./index.scss";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 
