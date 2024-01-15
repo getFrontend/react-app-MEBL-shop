69f9b7a3e96e8decc1a675e3d7ecb81eb9d39d5b
Sergey UP <sergey10goals@gmail.com>
2024-01-15 19:39:00 +0200

    Added rendering of icons in the catalog

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,18 +1,28 @@
+import { getCatalogIcon } from "../../helpers/getCatalogIcon";
 import { Container } from "../../views/Container/Container";
 import styles from "./Catalog.module.scss";
 
 function Catalog({ data }, ...props) {
-  // const defaultData = [
-  //   "Тумбы", "Стулья", "Столы", "Пуфы и банкетки", "Кровати", "Диваны", "Полки", "Стеллажи"
-  // ];
+  const defaultData = [
+    "Тумбы", "Стулья", "Столы", "Пуфы и банкетки", "Кровати", "Диваны", "Полки", "Стеллажи"
+  ];
+
+  const catalog = data || defaultData;
 
   return (
     <nav className={styles["catalog"]}>
       <Container className={styles["catalog__container"]}>
         <ul className={styles["catalog__list"]}>
-          {data.map((item, index) => (
+          {catalog.map((item, index) => (
             <li className={styles["catalog__item"]} key={index}>
-              <a className={styles["catalog__link"]} href={`/category?slug=${item}`}>{item}</a>
+              <a className={styles["catalog__link"]} href={`/category?slug=${item}`}>
+                <img
+                  className={styles["catalog__icon"]}
+                  src={getCatalogIcon(index)}
+                  alt={`Иконка: ${item}`}
+                />
+                <span className={styles["catalog__text"]}>{item}</span>
+              </a>
             </li>
           ))}
         </ul>
