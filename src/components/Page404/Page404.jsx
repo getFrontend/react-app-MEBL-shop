1fa602c8c4a7c240a8084b7c189946a265f80aa7
Sergey UP <sergey10goals@gmail.com>
2024-01-16 23:50:43 +0200

    Created a Page404 component for the erroneous page not found. Added styles and animation.

---
+++ src/components/Page404/Page404.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { getCatalogIcon } from "../../helpers/getCatalogIcon";
+import styles from "./Page404.module.scss";
+
+function Page404() {
+  const image = getCatalogIcon(1);
+
+  return (
+    <div className={styles["page404"]}>
+      <h3 className={styles["title"]}>
+        <img className={styles["image"]} src={image} alt="" />
+        <span>Ой! Ошибка 404...</span>
+      </h3>
+      <p className={styles["text"]}>Страницу, которую вы ищите — не существует! 🤷‍♂️</p>
+      <p className={styles["text"]}>
+        Попробуйте вернуться на <Link className={styles["link"]} to="/">главную страницу</Link>.
+      </p>
+    </div>
+  );
+}
+
+export default Page404;
