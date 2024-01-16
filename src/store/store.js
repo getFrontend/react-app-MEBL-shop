025b12acf4c51d812ee60fee63fd34a61e58189a
Sergey UP <sergey10goals@gmail.com>
2024-01-16 17:25:58 +0200

    added productReducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth.slice";
 import categoriesReducer from "./categories/categories.slice";
 import productsReducer from "./products/products.slice";
+import productReducer from "./product/product.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     categories: categoriesReducer,
-    products: productsReducer
+    products: productsReducer,
+    product: productReducer
   }
 });
