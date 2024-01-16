7f3965c9b4c4bbdcc83fae2389702ccdb38d262e
Sergey UP <sergey10goals@gmail.com>
2024-01-16 07:57:29 +0200

    changed categories & products import names

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth.slice";
-import authCategories from "./categories/categories.slice";
-import authProducts from "./products/products.slice";
+import categoriesReducer from "./categories/categories.slice";
+import productsReducer from "./products/products.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    categories: authCategories,
-    products: authProducts
+    categories: categoriesReducer,
+    products: productsReducer
   }
 });
