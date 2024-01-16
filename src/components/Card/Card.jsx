3dd02ef4490f83cf738b9cf074b158c8971052dc
Sergey UP <sergey10goals@gmail.com>
2024-01-16 09:33:45 +0200

    initialized Card and Cart components

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import styles from "./Card.module.scss";
+
+function Card() {
+  return (
+    <div className={styles["card"]}>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import styles from "./Cart.module.scss";
+
+function Cart() {
+  return (
+    <div className={styles["cart"]}>
+    </div>
+  );
+}
+
+export default Cart;
