7094f2b92e623dfe02bfa8ac2188e783d8d9fa62
Sergey UP <sergey10goals@gmail.com>
2024-01-16 12:48:11 +0200

    updated Cart Component: added title for test

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,12 @@
+import { Container } from "../../views/Container/Container";
 import styles from "./Cart.module.scss";
 
 function Cart() {
   return (
     <div className={styles["cart"]}>
+      <Container>
+        Cart
+      </Container>
     </div>
   );
 }
