c6fa3d033414a8a770f493756e5cad883a4010be
Sergey UP <sergey10goals@gmail.com>
2024-01-18 15:37:44 +0200

    Updated the Card component: added a check that data is not an empty object so that there are no critical errors; added an additional check that this data is there before rendering the gallery.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -20,20 +20,11 @@ function Card() {
     error: errorProduct,
   } = useSelector(state => state.product);
 
-  const {
-    name: title,
-    price,
-    article,
-  } = data;
-
-  console.log("data: ", data);
-
   useEffect(() => {
     dispatch(fetchProduct(productId));
   }, [dispatch, productId]);
 
-
-  if (loading) {
+  if (loading || Object.keys(data).length === 0) {
     return (
       <Loader />
     );
@@ -41,7 +32,7 @@ function Card() {
 
   if (errorProduct) {
     const errorData = {
-      path: "products",
+      path: "product",
       errorMsg: errorProduct
     };
     return (
@@ -52,31 +43,35 @@ function Card() {
   return (
     <section className={styles["card"]}>
       <Container className={styles["container"]}>
-        <h2 className={styles["title"]}>{title}</h2>
-
-        <Gallery {...data} />
-
-        <div className={styles["info"]}>
-          <p className={styles["price"]}>
-            {parseInt(price).toLocaleString()}&nbsp;
-            <span className={styles["price__currency"]}>₴</span></p>
-          <p className={styles["article"]}>арт. {article}</p>
-
-          <Specifications />
-
-          <div className={styles["buttons"]}>
-            <button className={styles["buttons-cart"]} data-id={productId}>В корзину</button>
-            <button className={styles["buttons-favourite"]} data-id={productId}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                {/* eslint-disable-next-line max-len */}
-                <path d="M8.41325 13.8733C8.18659 13.9533 7.81325 13.9533 7.58659 13.8733C5.65325 13.2133 1.33325 10.46 1.33325 5.79332C1.33325 3.73332 2.99325 2.06665 5.03992 2.06665C6.25325 2.06665 7.32659 2.65332 7.99992 3.55998C8.67325 2.65332 9.75325 2.06665 10.9599 2.06665C13.0066 2.06665 14.6666 3.73332 14.6666 5.79332C14.6666 10.46 10.3466 13.2133 8.41325 13.8733Z"
-                  stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
+        {data ?
+          (
+            <>
+              <h2 className={styles["title"]}>{data.name}</h2>
+              <Gallery data={data} />
+              <div className={styles["info"]}>
+                <p className={styles["price"]}>
+                  {parseInt(data.price).toLocaleString()}&nbsp;
+                  <span className={styles["price__currency"]}>₴</span></p>
+                <p className={styles["article"]}>арт. {data.article}</p>
+                <Specifications data={data} />
+                <div className={styles["buttons"]}>
+                  <button className={styles["buttons-cart"]} data-id={productId}>В корзину</button>
+                  <button className={styles["buttons-favourite"]} data-id={productId}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                      {/* eslint-disable-next-line max-len */}
+                      <path d="M8.41325 13.8733C8.18659 13.9533 7.81325 13.9533 7.58659 13.8733C5.65325 13.2133 1.33325 10.46 1.33325 5.79332C1.33325 3.73332 2.99325 2.06665 5.03992 2.06665C6.25325 2.06665 7.32659 2.65332 7.99992 3.55998C8.67325 2.65332 9.75325 2.06665 10.9599 2.06665C13.0066 2.06665 14.6666 3.73332 14.6666 5.79332C14.6666 10.46 10.3466 13.2133 8.41325 13.8733Z"
+                        stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>Данные не пришли</>
+          )
+        }
       </Container>
-    </section>
+    </section >
   );
 }
 
