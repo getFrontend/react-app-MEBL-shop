35b59ca8b94c9e0e29fd3cf677fefb6aa0434784
Sergey UP <sergey10goals@gmail.com>
2024-01-18 15:34:22 +0200

    updated the Specifications component: replaced the rendering of static data with dynamically rendered data.

--- src/components/Specifications/Specifications.jsx
+++ src/components/Specifications/Specifications.jsx
@@ -1,30 +1,18 @@
 import styles from "./Specifications.module.scss";
 
-function Specifications() {
+function Specifications({ data }) {
   return (
     <div className={styles["specs"]}>
       <h3 className={styles["specs-title"]}>Общие характеристики</h3>
       <table className={`${styles["specs-table"]} ${styles["table"]}`}>
-        <tr className={styles["table__row"]}>
-          <td className={styles["table__field"]}>Тип</td>
-          <td className={styles["table__value"]}>Компьютерный стол</td>
-        </tr>
-        <tr className={styles["table__row"]}>
-          <td className={styles["table__field"]}>Ширина, см</td>
-          <td className={styles["table__value"]}>108.1</td>
-        </tr>
-        <tr className={styles["table__row"]}>
-          <td className={styles["table__field"]}>Глубина, см</td>
-          <td className={styles["table__value"]}>47</td>
-        </tr>
-        <tr className={styles["table__row"]}>
-          <td className={styles["table__field"]}>Высота, см</td>
-          <td className={styles["table__value"]}>103.4</td>
-        </tr>
-        <tr className={styles["table__row"]}>
-          <td className={styles["table__field"]}>Бренд</td>
-          <td className={styles["table__value"]}>GARUN</td>
-        </tr>
+        <tbody>
+          {data.characteristics.map((item, index) => (
+            <tr className={styles["table__row"]} key={index}>
+              <td className={styles["table__field"]}>{item[0]}</td>
+              <td className={styles["table__value"]}>{item[1]}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
