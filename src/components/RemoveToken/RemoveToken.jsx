cd50b914ac321178d8ca6e94d4228bcc7ceb164e
Sergey UP <sergey10goals@gmail.com>
2024-01-18 10:00:30 +0200

    updated RemoveToken component: added a button to Home, updated styles for better adaptability on mobile devices

--- src/components/RemoveToken/RemoveToken.jsx
+++ src/components/RemoveToken/RemoveToken.jsx
@@ -1,10 +1,8 @@
 import { useSelector } from "react-redux";
 import "./RemoveToken.scss";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function RemoveToken() {
-  const navigate = useNavigate();
-
   const {
     accessToken,
   } = useSelector(state => state.auth);
@@ -13,7 +11,7 @@ function RemoveToken() {
     localStorage.removeItem("accessTokenMebl");
     console.log("✅Токен был успешно обновлён!!!");
     console.log("🐱‍🏍Перезагрузка страницы");
-    navigate("/");
+    location.reload(true);
   };
 
 
@@ -21,6 +19,7 @@ function RemoveToken() {
     <div className="wrapper">
       <div className="text"> <span className="token">{accessToken}</span></div>
       <button className="button-reset" onClick={reset}>Обновить токен</button>
+      <Link className="button-home" to="/">На главную</Link>
     </div>
   );
 }
