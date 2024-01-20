1874cfc29c7af89a2841720939cfa32aed91dfe4
Sergey UP <sergey10goals@gmail.com>
2024-01-20 17:08:00 +0200

    integrated the new ProductList component into Products; removed code and styles that were no longer used

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Container } from "../../views/Container/Container";
-import ProductCard from "../ProductCard/ProductCard";
 import styles from "./Products.module.scss";
 import { useEffect } from "react";
 import { fetchProducts } from "../../store/products/products.slice";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import ProductList from "../ProductList/ProductList";
 
 function Products(props) {
   const dispatch = useDispatch();
@@ -41,13 +41,7 @@ function Products(props) {
     <section className={styles["products"]}>
       <Container>
         <h2 className={`${styles["products__title"]}`}>Лучшие новинки каталога 2024</h2>
-        <ul className={styles["products__list"]}>
-          {data.map(item => (
-            <li className={styles["products__item"]} key={item.id}>
-              <ProductCard {...item} />
-            </li>
-          ))}
-        </ul>
+        <ProductList data={data} />
       </Container>
     </section>
   );
