db5ff37c3916f0ba5cd4a51ed9bb52fba39787bb
Sergey UP <sergey10goals@gmail.com>
2024-01-20 21:54:16 +0200

    created favoriteSlice to process a request to the favorite products list favoriteList stored in local storage

---
+++ src/store/favorites/favorites.slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  favoriteList: JSON.parse(localStorage.getItem("favoriteMEBL") || "[]")
+  // Тоже самое, что и строка выше
+  // favoriteList: localStorage.getItem("favoriteMEBL") ?
+  //   JSON.parse(localStorage.getItem("favoriteMEBL")) :
+  //   []
+};
+
+const favoriteSlice = createSlice(
+  {
+    name: "favorite",
+    initialState,
+    reducers: {
+      addFavorite: (state, action) => {
+        state.favoriteList.push(action.payload);
+        localStorage.setItem("favoriteMEBL", JSON.stringify(state.favoriteList));
+      },
+      removeFavorite: (state, action) => {
+        state.favoriteList = state.favoriteList.filter((id) => id !== action.payload);
+        localStorage.removeItem("favoriteMEBL", JSON.stringify(state.favoriteList));
+      }
+    }
+  }
+);
+
+export const { addFavorite, removeFavorite } = favoriteSlice.actions;
+
+export default favoriteSlice.reducer;
