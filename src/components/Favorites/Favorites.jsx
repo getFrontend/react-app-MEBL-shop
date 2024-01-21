539357c0148d8107f18a5df05653738e92d7cace
Sergey UP <sergey10goals@gmail.com>
2024-01-21 18:51:07 +0200

    updated the Favorites component: improved the visual and text part of the message to the user when the list of favorite products is empty

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../store/products/products.slice";
 import Products from "../Products/Products";
 import { Container } from "../../views/Container/Container";
+import { getCatalogIcon } from "../../helpers/getCatalogIcon";
 
 function Favorites() {
   const dispatch = useDispatch();
@@ -16,12 +17,23 @@ function Favorites() {
     }
   }, [dispatch, favoriteList]);
 
+  const image = getCatalogIcon(1);
+
   return <>
     {favoriteList?.length ? (
       <Products />
     ) : (
       <Container>
-        <div className={styles["empty"]}>Извините, но пока в избранном пусто!</div>
+        <div className={styles["empty"]}>
+          <img className={styles["image"]} src={image} alt="" />
+          <div className={styles["wrapper"]}>
+            <h2 className={styles["title"]}>Ой... <span>а в Избранном пусто 😢</span></h2>
+            <div className={styles["text"]}>
+              <p>Добавьте какой-нибудь товар, нажав на сердечко <span>💚</span></p>
+              <p>И здесь появятся все понравившиеся вам товары <span>😊</span></p>
+            </div>
+          </div>
+        </div>
       </Container>
     )}
   </>;
