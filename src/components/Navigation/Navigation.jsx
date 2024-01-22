49bfb53ad6b023eb3d4984d71d757b25ad3c3399
Sergey UP <sergey10goals@gmail.com>
2024-01-22 16:18:12 +0200

    updated Navigation component: improved visual focus when there are items in favorites

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 
 function Navigation(props) {
   const favoriteList = useSelector((state) => state.favorite.favoriteList);
+  const isActive = favoriteList?.length > 0;
+
 
   return (
     <nav className={`${styles["control"]} ${props.className}`}>
@@ -12,7 +14,7 @@ function Navigation(props) {
         <span className={styles["count"]}>
           ({favoriteList.length})
         </span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <svg className={isActive ? `${styles["active"]}` : ""} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
           {/* eslint-disable-next-line max-len */}
           <path d="M8.41325 13.8733C8.18659 13.9533 7.81325 13.9533 7.58659 13.8733C5.65325 13.2133 1.33325 10.46 1.33325 5.79332C1.33325 3.73332 2.99325 2.06665 5.03992 2.06665C6.25325 2.06665 7.32659 2.65332 7.99992 3.55998C8.67325 2.65332 9.75325 2.06665 10.9599 2.06665C13.0066 2.06665 14.6666 3.73332 14.6666 5.79332C14.6666 10.46 10.3466 13.2133 8.41325 13.8733Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
         </svg>
