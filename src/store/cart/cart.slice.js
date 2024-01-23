5a2264b4d1f9172b6cceee6de5c275cc2996d4cd
Sergey UP <sergey10goals@gmail.com>
2024-01-23 22:15:39 +0200

    created cartSlice to receive data about the number of items in the cart and send the data to the server

---
+++ src/store/cart/cart.slice.js
@@ -0,0 +1,135 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_URL } from "../api";
+
+export const fetchCart = createAsyncThunk(
+  "cart/fetchCart",
+  async (_, { getState, rejectWithValue }) => {
+    const state = getState();
+    const token = state.auth.accessToken;
+
+    try {
+      const response = await fetch(`${API_URL}api/cart`, {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      if (!response.ok) {
+        if (response.status === 404) {
+          console.error("❌ Ошибка 404: Не удалось загрузить данные для корзины.");
+          return rejectWithValue({
+            status: response.status,
+            error: "Ошибка 404: Не удалось загрузить данные для корзины."
+          });
+        }
+        throw new Error("Ошибка! Не удалось загрузить данные для корзины!");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addProductToCart = createAsyncThunk(
+  "cart/addProductToCart",
+  async (productData, { getState, rejectWithValue }) => {
+    const state = getState();
+    const token = state.auth.accessToken;
+
+    try {
+      const response = await fetch(`${API_URL}api/cart/products`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(productData)
+      });
+      if (!response.ok) {
+        throw new Error("Ошибка! Не удалось добавить товар в корзину.");
+      }
+      return await response.json();
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateProductToCart = createAsyncThunk(
+  "cart/updateProductToCart",
+  async (productData, { getState, rejectWithValue }) => {
+    const state = getState();
+    const token = state.auth.accessToken;
+
+    try {
+      const response = await fetch(`${API_URL}api/cart/products`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(productData)
+      });
+      if (!response.ok) {
+        throw new Error("Ошибка! Не удалось обновить корзину.");
+      }
+      return await response.json();
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const initialState = {
+  products: [],
+  totalPrice: 0,
+  totalCount: 0,
+  loadingFetch: false,
+  loadingAdd: false,
+  loadingUpdate: false,
+  loadingRemove: false,
+  error: null
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCart.pending, (state) => {
+        state.loadingFetch = true;
+        state.error = null;
+      })
+      .addCase(fetchCart.fulfilled, (state, action) => {
+        state.products = action.payload.products;
+        state.totalPrice = action.payload.totalPrice;
+        state.totalCount = action.payload.totalCount;
+        state.loadingFetch = false;
+        state.error = null;
+      })
+      .addCase(fetchCart.rejected, (state, action) => {
+        state.loadingFetch = false;
+        state.error = action.error.message;
+      })
+      .addCase(addProductToCart.pending, (state) => {
+        state.loadingAdd = true;
+        state.error = null;
+      })
+      .addCase(addProductToCart.fulfilled, (state, action) => {
+        state.products = action.payload.products;
+        state.totalPrice = action.payload.totalPrice;
+        state.totalCount = action.payload.totalCount;
+        state.loadingAdd = false;
+        state.error = null;
+      })
+      .addCase(addProductToCart.rejected, (state, action) => {
+        state.loadingAdd = false;
+        state.error = action.error.message;
+      });
+  }
+});
+
+
+export default cartSlice.reducer;
