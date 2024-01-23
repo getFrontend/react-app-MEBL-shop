3187a7a6c53a8aa8efc7a6dc2b2e2892775fb2b2
Sergey UP <sergey10goals@gmail.com>
2024-01-23 22:25:47 +0200

    Updated Cart component: integrated CartProducts, CartPlace, CartForm; added output of dynamic data.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Container } from "../../views/Container/Container";
-import styles from "./Cart.module.scss";
+import CartForm from "../CartForm/CartForm";
+import CartPlace from "../CartPlace/CartPlace";
+import CartProducts from "../CartProducts/CartProducts";
+import s from "./Cart.module.scss";
+import { useEffect } from "react";
+import { fetchCart } from "../../store/cart/cart.slice";
+import Loader from "../Loader/Loader";
+// import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 function Cart() {
+  const dispatch = useDispatch();
+  const {
+    products,
+    totalPrice,
+    totalCount,
+    loadingFetch,
+    // error: errorCart
+  } = useSelector(state => state.cart);
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+
+  if (loadingFetch) {
+    return <Loader />;
+  }
+
+  // if (errorCart) {
+  //   return <ErrorMessage errorMsg={errorCart || ""} />;
+  // }
+
   return (
-    <div className={styles["cart"]}>
-      <Container>
-        Cart
-      </Container>
-    </div>
+    <section className={s.cart}>
+      {products ? (<>
+        <Container className={s.container}>
+          <h2 className={s.title}>Корзина</h2>
+          <CartProducts data={products} />
+          <CartPlace totalPrice={totalPrice} totalCount={totalCount} />
+          <CartForm />
+        </Container>
+      </>) : <Container>В корзине сейчас пусто...</Container>}
+    </section >
   );
 }
 
