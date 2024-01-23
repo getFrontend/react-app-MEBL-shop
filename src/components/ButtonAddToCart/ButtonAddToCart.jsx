2865a4bee485a5e27cb48c73e8fe675cff55a910
Sergey UP <sergey10goals@gmail.com>
2024-01-23 22:27:19 +0200

    created a button component that adds an item to cart

---
+++ src/components/ButtonAddToCart/ButtonAddToCart.jsx
@@ -0,0 +1,40 @@
+import { useDispatch } from "react-redux";
+import { addProductToCart, fetchCart } from "../../store/cart/cart.slice";
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+
+function ButtonAddToCart({ className, id }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+
+  const toast = useToast();
+  const addCartToast = () => {
+    toast({
+      description: "Товар добавлен в корзину!",
+      status: "success",
+      variant: "left-accent",
+      duration: 2000,
+      isClosable: true,
+    });
+  };
+
+  const handleCartClick = () => {
+    dispatch(addProductToCart({ productId: id, quantity: 1 }));
+    addCartToast();
+  };
+
+  return (
+    <button
+      onClick={handleCartClick}
+      className={className}
+      data-id={id}
+      aria-label="Добавить в корзину">
+      В корзину
+    </button>
+  );
+}
+
+export default ButtonAddToCart;
