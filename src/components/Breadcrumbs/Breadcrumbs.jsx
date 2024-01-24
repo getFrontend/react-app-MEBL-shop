228a39f8d1643a1443369a0c567f135e880a12ca
Sergey UP <sergey10goals@gmail.com>
2024-01-24 12:21:59 +0200

    initialized: Breadcrumbs component to create a block with breadcrumbs navigation

---
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,13 @@
+import styles from "./Breadcrumbs.module.scss";
+
+function Breadcrumbs() {
+  return (
+    <>
+      <div className={styles[""]}>
+        <div className={styles[""]}></div>
+      </div>
+    </>
+  );
+}
+
+export default Breadcrumbs;
