930a1e74be5d4ab9ea774004bc4f7167ff5eb6e1
Sergey UP <sergey10goals@gmail.com>
2024-01-26 20:08:10 +0200

    fixed: replaced h1 with h2 for Google SEO so that the header elements are sequenced in descending order, since most headers on the site are third level h3

--- src/components/TitleMain/TitleMain.jsx
+++ src/components/TitleMain/TitleMain.jsx
@@ -2,9 +2,9 @@ import s from "./TitleMain.module.scss";
 
 function TitleMain({ className, title }) {
   return (
-    <h1 className={className ? `${className}` : s.title}>
+    <h2 className={className ? `${className}` : s.title}>
       {title}
-    </h1 >
+    </h2>
   );
 }
 
