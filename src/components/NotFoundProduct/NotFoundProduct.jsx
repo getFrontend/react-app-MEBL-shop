fd0ae5e0e745d0a5c90a1c26e92fffdc246d7d42
Sergey UP <sergey10goals@gmail.com>
2024-01-27 22:06:31 +0200

    updated NotFoundProduct component: added animation if nothing was found in the search for the given query

--- src/components/NotFoundProduct/NotFoundProduct.jsx
+++ src/components/NotFoundProduct/NotFoundProduct.jsx
@@ -1,13 +1,30 @@
 import { Container } from "../../views/Container/Container";
 import styles from "./NotFoundProduct.module.scss";
+import { useLottie } from "lottie-react";
+import searchAnimation from "../../lotties/search.json";
 
 function NotFoundProduct({ search }) {
+  const options = {
+    speed: 0.5,
+    autoplay: true,
+    animationData: searchAnimation,
+    loop: true,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  const { View } = useLottie(options);
+
   return (
     <Container>
       <div className={styles["not-found"]}>
         <h3 className={styles["title"]}>Ой...</h3>
         <div className={styles["text"]}>Увы, но мы ничего не нашли по вашему запросу:</div>
-        <div className={styles["query"]}> {search}</div>
+        <div className={styles["query"]}>{search}</div>
+        <div className={styles.animation}>
+          <>{View}</>
+        </div>
       </div>
       <div className={styles["info"]}>
         <p>Возможно такого товара еще нет в нашем каталоге.</p>
