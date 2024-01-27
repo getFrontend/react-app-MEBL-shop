8e40ecd1a798a17bbf849c159e03ea7604242168
Sergey UP <sergey10goals@gmail.com>
2024-01-27 02:49:55 +0200

    integrated Hero component, added its rendering to Main

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -4,9 +4,20 @@ import Favorites from "../../components/Favorites/Favorites";
 import Cart from "../../components/Cart/Cart";
 import Card from "../../components/Card/Card";
 import Page404 from "../../components/Page404/Page404";
+import Hero from "../Hero/Hero";
 
 export const Main = ({ path }) => {
-  if (path === "home" || path === "category" || path === "search") {
+  if (path === "home") {
+    return (
+      <>
+        <Hero />
+        <Catalog />
+        <Products />
+      </>
+    );
+  }
+
+  if (path === "category" || path === "search") {
     return (
       <>
         <Catalog />
