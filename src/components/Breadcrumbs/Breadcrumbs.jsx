d46e67d3803b91b196261c6de6036428d4f62123
Sergey UP <sergey10goals@gmail.com>
2024-01-27 12:25:25 +0200

    updated the Breadcrumbs component and added all the functionality to form site navigation

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,13 +1,81 @@
-import styles from "./Breadcrumbs.module.scss";
+import s from "./Breadcrumbs.module.scss";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Container } from "../../views/Container/Container";
 
 function Breadcrumbs() {
-  return (
-    <>
-      <div className={styles[""]}>
-        <div className={styles[""]}></div>
+  const { data, loading } = useSelector((state) => state.product);
+  const { pathname } = useLocation();
+  const [searchParam] = useSearchParams();
+  const breadcrumbs = [{ name: "Главная", link: "/" }];
+
+  if (pathname === "/category") {
+    const category = searchParam.get("category");
+    breadcrumbs.push({
+      name: category,
+      link: "",
+    });
+  }
+
+  if (pathname.split("/")[1] === "product") {
+    breadcrumbs.push(
+      {
+        name: data?.category,
+        link: `/category?category=${data?.category}`,
+      },
+      {
+        name: data?.name,
+        link: "",
+      },
+    );
+  }
+
+  if (pathname === "/favorite") {
+    breadcrumbs.push({
+      name: "Избранное",
+      link: "",
+    });
+  }
+
+  if (pathname === "/cart") {
+    breadcrumbs.push({
+      name: "Корзина",
+      link: "",
+    });
+  }
+
+  if (pathname === "/search") {
+    const search = searchParam.get("q");
+    breadcrumbs.push({
+      name: `Поиск по запросу: ${search}`,
+      link: `${pathname}`,
+    });
+  }
+
+  return <>
+    {!loading ? (
+      <div className={s.breadcrumb}>
+        <Container>
+          <ul className={s.list}>
+            {breadcrumbs?.map((item, index) => (
+              <li key={`bc${index}`} className={s.item}>
+                <Link
+                  className={
+                    breadcrumbs.length - 1 !== index ?
+                      s.link :
+                      `${s.link} ${s.disabled}`
+                  }
+                  to={item?.link}>
+                  {item?.name}
+                </Link>
+                <span className={s.separator}>➡</span>
+              </li>
+            ))}
+          </ul>
+        </Container>
       </div>
-    </>
-  );
+    ) : <div className={s.loading}></div>}
+  </>;
 }
 
 export default Breadcrumbs;
