d8735dd87d29ae92e381cef1310a28f582841684
Sergey UP <sergey10goals@gmail.com>
2024-01-28 15:34:30 +0200

    imported orderReducer into configureStore

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ import productsReducer from "./products/products.slice";
 import productReducer from "./product/product.slice";
 import favoriteReducer from "./favorites/favorites.slice";
 import cartReducer from "./cart/cart.slice";
+import orderReducer from "./order/order.slice";
 import { apiTokenErrorMiddleware } from "./middleware";
 
 export const store = configureStore({
@@ -14,7 +15,8 @@ export const store = configureStore({
     products: productsReducer,
     product: productReducer,
     favorite: favoriteReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    order: orderReducer
   },
   middleware: (getDefaultMiddleWare) =>
     getDefaultMiddleWare().concat(apiTokenErrorMiddleware)
